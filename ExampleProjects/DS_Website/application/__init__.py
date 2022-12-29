13a7ad164961aeaf18ce9f144ffb62430b5055ae
a-appelblom <a.appelblom@gmail.com>
2022-12-29 12:58:03 +0100

    base for website

---
+++ ExampleProjects/DS_Website/application/__init__.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'db_website.sqlite'),
+    )
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    @app.route('/fisk')
+    def fisk():
+        return 'Jag är en blåmes!'
+
+
+
+    return app
