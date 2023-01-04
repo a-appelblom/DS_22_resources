0c2c7a1c486826adc85bd53e42b274429eb417cb
Anton Appelblom <a.appelblom@gmail.com>
2023-01-04 16:30:51 +0100

    Adds higher lower base to lesson2 sthlm

---
+++ lessons/lesson2STHLM/higher_lower.py
@@ -0,0 +1,23 @@
+import random
+
+# Planen är att ni ska bygga ett litet snabbt gissningsspel med lite assistans. Det kommer att gå riktigt fint.
+
+# Det finns 4 delar som är viktiga här.
+# 1 Ett slumpat tal för varje
+# 2 Ett input från användaren
+# 3 En loop som håller spelet igång
+# 4 Ett block med if satser som kollar om det är högre eller lägre
+
+# Sen Är det alltid trevligt med lite scores så om man gissar rätt kan spelet fortsätta med en score som tickar upp tills det förloras.
+
+# Ett random nummer kan enkelt implementeras med en funktion som heter random, men den måste importeras.
+# Jag löser det år er här tills vi pratar importer senare
+
+# Förslag skapa en min och en max variabel så är det lätt att justera i efterhand.
+# max:int = int()
+# min:int = int()
+# random_num = random.randint(min, max) Denna funktion kommer att ge er ett nummer mellan 2 tal, lägsta först största sist. Vi pratar funktioner nästa gång.
+# ni behöver input. Det funkar som print funktionen men den väntar tills användaren skriver något och ger sedan en return med det värdet i en string.
+# guess = input("Vad är din gissning?: ")
+
+# Med dess funktioner och tips så går det att implementera detta enkla spel. Det tar lite tankekraft första gången men när man lär sig programmeringstänket så går det rätt snabbt.
