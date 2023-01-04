89eebaa1f5e84e1297e6fc03785ca3fb906d835e
Anton Appelblom <a.appelblom@gmail.com>
2023-01-04 10:11:46 +0100

    adds sthlm lesson 2 with base datatypes

---
+++ lessons/lesson2STHLM/base_types.py
@@ -0,0 +1,140 @@
+# Grundläggande datatyperna
+print("")
+print("______________Datatyper___________________")
+
+a_whole_number: int = 25
+a_floating_point_number: float = 2.5
+a_string: str = "Anton's, world!"
+a_boolean: bool = True  # False
+a_whole_lotta_a_nothin: None = None
+
+print(f"Integer: {a_whole_number} - Type: {type(a_whole_number)}")
+print(
+    f"Float: {a_floating_point_number} - Type: {type(a_floating_point_number)}")
+print(f"String: {a_string} - Type: {type(a_string)}")
+print(f"Boolean: {a_boolean} - Type: {type(a_boolean)}")
+print(
+    f"None: {a_whole_lotta_a_nothin} - Type: {type(a_whole_lotta_a_nothin)}")
+
+# Aritmetik
+# INT
+# En int vid operationer addition. subtration, multiplikation
+# En operation där andra talet är en float blir en float och även division mellan 2 integers blir en float
+print("")
+print("______________INT___________________")
+
+int1 = 10
+int2 = 5
+test_float = 2.5
+
+int3 = int1 + int2
+print(int3, type(int3))
+
+int3 = int2 - int1
+print(int3, type(int3))
+
+int3 = int1 * int2
+print(int3, type(int3))
+
+int3 = int1 / int2
+print(int3, type(int3))
+
+int3 = int1 + test_float
+print(int3, type(int3))
+
+# Float
+# All aritmetik med floats resulterar i en float
+print("")
+print("______________Float___________________")
+
+float1 = 2.0
+float2 = 1.5
+
+float3 = float1 + float2
+print(float3, type(float3))
+
+float3 = float2 - float1
+print(float3, type(float3))
+
+float3 = float1 * float2
+print(float3, type(float3))
+
+float3 = float1 / float2
+print(float3, type(float3))
+
+float3 = int1 + float1
+print(float3, type(float3))
+
+# String
+print("")
+print("______________String___________________")
+
+string1 = "Anton"
+string2 = 'Hej'
+
+string3 = string1 + string2
+print(string3, type(string3))
+
+# Funkar inte
+# string3 = string2 - string1
+# print(string3, type(string3))
+
+# Funkar inte
+# string3 = string1 * string2
+# print(string3, type(string3))
+
+string3 = string1 * int1
+print(string3, type(string3))
+
+# Funkar inte
+# string3 = string1 / string2
+# print(string3, type(string3))
+
+# Funkar inte
+# string3 = int1 + string1
+# print(string3, type(string3))
+
+# Funkar inte
+# string3 = float1 + string1
+# print(string3, type(string3))
+
+# Booleans
+print("")
+print("______________Booleans___________________")
+
+bool1 = True
+bool2 = False
+
+bool3 = bool1 + bool2
+print(bool3, type(bool3))
+
+bool3 = bool2 - bool1
+print(bool3, type(bool3))
+
+bool3 = bool1 * bool2
+print(bool3, type(bool3))
+
+# Funkar inte eftersom det är division med 0
+# bool3 = bool1 / bool2
+# print(bool3, type(bool3))
+
+bool3 = bool2 / bool1
+print(bool3, type(bool3))
+
+bool3 = int1 + bool1
+print(bool3, type(bool3))
+
+bool3 = float1 + bool1
+print(bool3, type(bool3))
+
+
+# None
+# Inga operationer
+print("")
+print("______________None___________________")
+
+none1 = None
+
+# Funkar inte
+# test = int1 + none1
+# print(test)
---
+++ lessons/lesson2STHLM/variables.py
@@ -0,0 +1,13 @@
+# Variabler
+# En variabel sparar ett värde
+# En variabel får inte börja med siffra, får inte innehålla mellanrum, skrivs med snake case.
+# Icke okay: 1a, a-b-c, antonAppelblom
+
+a = "Hello World!"
+greeting = "Hello"
+b = 1
+a1 = "Hello World"
+user_first_name = "Anton"
+# print = 1 !!! Inte bra
+
+print(a, b, a1, user_first_name)
