2f04d487720f64e529f5688fb0c6c16efd92c5a3
Anton Appelblom <a.appelblom@gmail.com>
2023-01-16 08:28:30 +0100

    adds questions file

---
+++ questions.py
@@ -0,0 +1,14 @@
+# Hur fungerar pop funktionen på en lista.
+
+people = ["Anton", "Erik", "Maja", "Amanda"]
+print(people)
+
+# Efter pop finns inte elementet kvar i listan
+# Men det returneras och kan sparas i en variabel.
+person = people.pop()
+
+print(people)
+print(person)
+
+people.append("Sven")
+print(people)
