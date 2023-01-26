4afc61120aa32a44c477cb8a62a015498e572b2c
Anton Appelblom <a.appelblom@gmail.com>
2023-01-26 14:03:01 +0100

    Adds sqlalchemy demo part 2

--- lessons/lesson8GBG/sqlalchemy_demo.py
+++ lessons/lesson8GBG/sqlalchemy_demo.py
@@ -1,6 +1,44 @@
-from sqlalchemy import create_engine
+from sqlalchemy import (
+    create_engine,
+    Table,
+    Column,
+    Integer,
+    String,
+    MetaData,
+    ForeignKey,
+    insert,
+    select,
+)
 
 engine = create_engine("sqlite:///test.db")
+meta_data = MetaData()
+
+alchemist_table = Table(
+    "alchemist",
+    meta_data,
+    Column("id", Integer, primary_key=True),
+    Column("person_id", ForeignKey("person.id")),
+)
+person_table = Table(
+    "person",
+    meta_data,
+    Column("id", Integer, primary_key=True),
+    Column("first_name", String),
+    Column("last_name", String),
+    Column("motto", String),
+)
+
+meta_data.create_all(engine)
+
+# statement = select(person_table).values(
+#     first_name="Anton", last_name="Appelblom", motto="Live damn you tele2"
+# )
+statement = select(person_table).where(person_table.c.first_name == "Anton")
+
+with engine.connect() as conn:
+    res = conn.execute(statement)
+    data = res.fetchall()
+    print(data)
 
 # connection = engine.connect()
 
@@ -19,41 +57,41 @@ engine = create_engine("sqlite:///test.db")
 
 # connection.close()
 
-with engine.connect() as conn:
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS person (id INTEGER PRIMARY KEY,  first_name VARCHAR(80), last_name VARCHAR(80), motto VARCHAR(255))"
-    )
-with engine.connect() as conn:
-    conn.execute(
-        "INSERT INTO person ( first_name, last_name, motto) VALUES ('Pikachu', 'Pikasson', 'Pika pika')"
-    )
+# with engine.connect() as conn:
+#     conn.execute(
+#         "CREATE TABLE IF NOT EXISTS person (id INTEGER PRIMARY KEY,  first_name VARCHAR(80), last_name VARCHAR(80), motto VARCHAR(255))"
+#     )
+# with engine.connect() as conn:
+#     conn.execute(
+#         "INSERT INTO person ( first_name, last_name, motto) VALUES ('Pikachu', 'Pikasson', 'Pika pika')"
+#     )
 
-with engine.connect() as conn:
-    first_name = "Al"
-    last_name = "Appelblom"
-    res = conn.execute(
-        "SELECT * FROM person WHERE first_name = ? OR last_name = ?",
-        (first_name, last_name),
-    )
-    data = res.fetchall()
-    print(data)
-
-
-def call_db_sqla(query: str, *args):
-    with engine.connect() as conn:
-        res = conn.execute(query, args)
-        if res:
-            data = res.fetchall()
-            return data
+# with engine.connect() as conn:
+#     first_name = "Al"
+#     last_name = "Appelblom"
+#     res = conn.execute(
+#         "SELECT * FROM person WHERE first_name = ? OR last_name = ?",
+#         (first_name, last_name),
+#     )
+#     data = res.fetchall()
+#     print(data)
 
 
-data = call_db_sqla("SELECT * FROM person WHERE first_name = ?", "Pikachu")
-print(data)
+# def call_db_sqla(query: str, *args):
+#     with engine.connect() as conn:
+#         res = conn.execute(query, args)
+#         if res:
+#             data = res.fetchall()
+#             return data
 
-data = call_db_sqla(
-    "INSERT INTO person (first_name, last_name, motto) VALUES ( ?, ?, ?)",
-    "Sven Bertil",
-    "Taube",
-    "Vet Faktiskt inte",
-)
-print(data)
+
+# data = call_db_sqla("SELECT * FROM person WHERE first_name = ?", "Pikachu")
+# print(data)
+
+# data = call_db_sqla(
+#     "INSERT INTO person (first_name, last_name, motto) VALUES ( ?, ?, ?)",
+#     "Sven Bertil",
+#     "Taube",
+#     "Vet Faktiskt inte",
+# )
+# print(data)
